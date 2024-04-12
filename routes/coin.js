45c4a44809974bac2444a7b2365a128ddb8b7189
Valentina Ruiz <valentinaruito@unisabana.edu.co>
2024-04-11 23:03:31 -0500

    commit20

--- routes/coin.js
+++ routes/coin.js
@@ -28,9 +28,9 @@ router.get("/:coinName", async (req, res) => {
   const price = await fetchCoinPrice(coinName);
 
   if (price !== null) {
-    res.send(`El precio en USD de ${coinName} para el día de hoy es ${price}`);
+    res.send(`El precio en dólares de ${coinName} para el día de hoy es ${price}`);
   } else {
-    res.status(400).send("Error de entrada: Nombre de la moneda no se encuentra o es incorrecto");
+    res.status(400).send("Error de entrada: Nombre de la moneda no se encuentra en la base de datos o es incorrecto");
   }
 });
 
